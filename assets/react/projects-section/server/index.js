433f42a49f6c83f81eea80ec643e72887b0c8564
Mason Jacob <masonjacob@uri.edu>
2022-06-20 10:05:37 -0400

    updated react

---
+++ assets/react/projects-section/server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'MysticQuills|10|',
+    database: 'portfolio-website-database'
+});
+
+app.post('/post', (req, res) => {
+    const id  = req.body.id;
+    const name = req.body.name;
+    const description = req.body.description;
+    const image = req.body.image;
+    db.query(
+        'INSERT INTO projects (id, name, description, image) VALUES (?,?)', 
+        [id, name, description, image], 
+        (err, result) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.send("Values Inserted");
+            }
+        }
+    );
+});
+
+app.get("/api", (req,res) => {
+    res.json({message: "Hello World!"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
