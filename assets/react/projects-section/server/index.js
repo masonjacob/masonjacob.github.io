af1c1698d83ea0e396da27fb851545788b7e8df3
Mason Jacob <masonjacob@uri.edu>
2022-06-21 15:55:43 -0400

    Updated

--- assets/react/projects-section/server/index.js
+++ assets/react/projects-section/server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -19,7 +19,7 @@ app.use(express.json());
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
-  });*/
+  });
 
 const db = mysql.createConnection({
     user: 'root',
