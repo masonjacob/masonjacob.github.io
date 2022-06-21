984cd9c0a2b8ab6f100b6ef7c068c70aff49470d
Mason Jacob <masonjacob@uri.edu>
2022-06-21 11:58:12 -0400

    Updated Server

--- assets/react/projects-section/server/index.js
+++ assets/react/projects-section/server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
+
+app.use(cors());
+// app.use(express.json());
 
 const db = mysql.createConnection({
     user: 'root',
