268230977365ab585b64e37cdfd206df2c362722
Mason Jacob <masonjacob@uri.edu>
2022-06-21 16:09:39 -0400

    Updates

--- assets/react/projects-section/server/index.js
+++ assets/react/projects-section/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
