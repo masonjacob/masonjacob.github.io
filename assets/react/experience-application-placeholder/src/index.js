c6bff7876d09d47b7fc9098beece11723679a09e
Mason Jacob <masonjacob@uri.edu>
2022-07-03 09:37:45 -0400

    Updates

---
+++ assets/react/experience-application-placeholder/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/css/react.css';
+import Loading from './components/Loading';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Loading type={"bars"} color={"#6f199d"}/>
+  </React.StrictMode>
+);
